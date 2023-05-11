ed814e49064bdf6e790d140f795a439cb30044c7
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-05-11 09:07:37 -0700

    create: parser

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <string.h>
+
+/* parser */
+char **parser(char *command);
+int argcnt(char *command);
+
+#endif
+
---
+++ parser.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * parser - parses the @command
+ * @command: input command
+ *
+ * Return: pointer to parsed command
+ */
+
+char **parser(char *command)
+{
+	int i, argc, argv;
+
+	if (!command)
+		return (NULL);
+
+	argc = argcnt(command);
+	argv = malloc(sizeof(char *) * argc);
+	if (!argv)
+		return (NULL);
+
+	ragv[0] = strtok(command, " ");
+	for (i = 1; i < argc; i++)
+		argv[i] = strtok(NULL, " ");
+
+	return (argv);
+}
+
+/**
+ * argcnt - counts the number of arguments
+ * @command: stirng commands
+ *
+ * Return: number of arguments
+ */
+
+int argcnt(char *command)
+{
+	int i, count = 1;
+
+	for (i = 0; !command[i]; i++)
+		if (command[i] == ' ')
+			count++;
+}
+
