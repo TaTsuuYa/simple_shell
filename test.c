99630b1470c43a9f40c0b05dce2419fd1cee4e87
ayoub <ayoubaitelaouad@gmail.com>
2023-05-11 21:32:17 +0100

    fixed compalition issue

--- test.c
+++ test.c
@@ -1,13 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 
 int main(int argc, char **argv, char **env)
 {
 	char *arr[] = {"/bin/ls", "-l", NULL};
+  int Result;
 
 	(void) argc;
 	(void) argv;
 
-	executor(arr, env);
+	Result = executor(arr, env);
+  printf("executor return value = %d\n", Result);
 
 	return (0);
 }
